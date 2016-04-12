2c1862c43672ce57a5f4da4e36f5b86138bc8c2b
Pedro Bras <pedro.bras@gen.pt>
2016-04-12 18:42:17 +0100

    Refactor scrolling

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
-module.exports = function(element, to, duration) {
-  var difference = to - element.scrollTop
-  var perTick = difference / duration * 10
+module.exports = function(element, target, duration) {
+  target = Math.round(target);
+  duration = Math.round(duration);
 
-  setTimeout(function() {
-    element.scrollTop = element.scrollTop + perTick
+  if (duration < 0) {
+    return Promise.reject("bad duration");
+  }
 
-    if (element.scrollTop === to) return
+  if (duration === 0) {
+      element.scrollTop = target;
+      return Promise.resolve();
+  }
 
-    scrollTo(element, to, duration - 10)
-  }, 10)
+  var start_time = Date.now();
+  var end_time = start_time + duration;
+
+  var start_top = element.scrollTop;
+  var distance = target - start_top;
+
+  var smooth_step = function(start, end, point) {
+    if(point <= start) { return 0; }
+    if(point >= end) { return 1; }
+    var x = (point - start) / (end - start); // interpolation
+    return x*x*(3 - 2*x);
+  }
+
+  return new Promise(function(resolve, reject) {
+    var previous_top = element.scrollTop;
+
+    var scroll_frame = function() {
+      if(element.scrollTop != previous_top) {
+        reject("interrupted");
+        return;
+      }
+
+      var now = Date.now();
+      var point = smooth_step(start_time, end_time, now);
+      var frameTop = Math.round(start_top + (distance * point));
+
+      element.scrollTop = frameTop;
+
+      if(now >= end_time) {
+        resolve();
+        return;
+      }
+
+      if(element.scrollTop === previous_top
+        && element.scrollTop !== frameTop) {
+        resolve();
+        return;
+      }
+
+      previous_top = element.scrollTop;
+
+      setTimeout(scroll_frame, 0);
+    }
+
+    setTimeout(scroll_frame, 0);
+  });
 }
